1f5c9624eb6a46b9f7e95aad41bc531c5112ef1f
Emine Yıldırım <emineyildirim2526@gmail.com>
2025-08-23 22:00:56 +0300

    migrations

--- obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f0b64a9120b070ba4f2c370f23c2f1ce48d5d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
